30029967b18bad0af561ab1666ae3188c870121f
gabewitmer <gabewitmer@yahoo.com>
2015-03-02 11:05:23 -0500

    cleric update

--- angularCleric/controllers.js
+++ angularCleric/controllers.js
@@ -10,47 +10,86 @@ characterControllers.controller('ClericController', ['$scope', 'ClericService', 
 		}
 	);
 
-	ClericService.get({phoneId: 1});
-
-	$scope.getMaxHD = function(roll, chaMod, clericLevel) {
-		var check = roll + chaMod;
+	function getMaxHd() {
+		var check = $scope.roll + $scope.chaMod;
+		if ($scope.religion) {
+			check += 2;
+		}
 		switch(true) {
 			case (check < 1):
-				return (clericLevel - 4);
+				$scope.maxHd = ($scope.cleric.level - 4);
+				return;
 			case (check > 0 && check < 4):
-				return (clericLevel - 3);
+				$scope.maxHd = ($scope.cleric.level - 3);
+				return;
 			case (check > 3 && check < 7):
-				return (clericLevel - 2);
+				$scope.maxHd = ($scope.cleric.level - 2);
+				return;
 			case (check > 6 && check < 10):
-				return (clericLevel - 1);
+				$scope.maxHd = ($scope.cleric.level - 1);
+				return;
 			case (check > 9 && check < 13):
-				return (clericLevel);
+				$scope.maxHd = ($scope.cleric.level);
+				return;
 			case (check > 12 && check < 16):
-				return (clericLevel + 1);
+				$scope.maxHd = ($scope.cleric.level + 1);
+				return;
 			case (check > 15 && check < 19):
-				return (clericLevel + 2);
+				$scope.maxHd = ($scope.cleric.level + 2);
+				return;
 			case (check > 18 && check < 22):
-				return (clericLevel + 3);	
+				$scope.maxHd = ($scope.cleric.level + 3);
+				return;
 			case (check > 21):
-				return (clericLevel + 4);
+				$scope.maxHd = ($scope.cleric.level + 4);
+				return;
 		}
+
 	};
 
-	$scope.getDmgRoll = function(chaMod, clericLevel) {
+	function getDmgRoll() {
 		var dmgRoll = Math.floor((Math.random()*11) + 2);
-		$scope.dmgRoll = (chaMod + clericLevel + dmgRoll);
+		$scope.dmgRoll = ($scope.chaMod + $scope.cleric.level + dmgRoll);
 		$scope.dmgLeft = $scope.dmgRoll;
+	};
+
+	$scope.calculate = function() {
+		getMaxHd();
+		getDmgRoll();
 		$scope.result = true;
+		$scope.img = 'clericturning.jpg';
 	};
 
 	$scope.updateDmgLeft = function(creatureHD) {
 		$scope.dmgLeft = $scope.dmgLeft - creatureHD;
 	};
 
+	$scope.change = function() {
+		//$scope.img = 'cleric.jpg';
+		switch ($scope.counter) {
+			case 0:
+				$scope.img = 'cleric.jpg';
+				$scope.counter++;
+				break;
+			case 1:
+				$scope.img = 'clericturning.jpg';
+				$scope.counter++;
+				break;
+			case 2:
+				$scope.img = 'cleric3.jpg';
+				$scope.counter = 0;
+				break;
+		}
+	}
+
 	$scope.dmgLeft = 0;
 
 	$scope.result = false;
 
 	$scope.dmgRoll = 0;
+
+	$scope.img = 'cleric.jpg';
+
+	$scope.counter = 1;
 
 }]);
--- angularCleric/services.js
+++ angularCleric/services.js
@@ -3,9 +3,5 @@
 var services = angular.module('services', ['ngResource']);
 
 services.factory('ClericService', ['$resource', function($resource) {
-	return $resource('data/:phoneId.json');
-}]);
-
-services.factory('FighterService', ['$resource', function($resource) {
-	return $resource('fighter.json');
+	return $resource('cleric.json');
 }]);
